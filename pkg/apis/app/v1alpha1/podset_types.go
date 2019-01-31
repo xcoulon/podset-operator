f13a90c5706fb7abc433691c67f2b44a070c8500
Xavier Coulon <xcoulon@redhat.com>
2019-01-31 16:21:16 +0100

    scaffold API and controller
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

---
+++ pkg/apis/app/v1alpha1/podset_types.go
@@ -0,0 +1,45 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// PodSetSpec defines the desired state of PodSet
+type PodSetSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+}
+
+// PodSetStatus defines the observed state of PodSet
+type PodSetStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// PodSet is the Schema for the podsets API
+// +k8s:openapi-gen=true
+type PodSet struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   PodSetSpec   `json:"spec,omitempty"`
+	Status PodSetStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// PodSetList contains a list of PodSet
+type PodSetList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []PodSet `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&PodSet{}, &PodSetList{})
+}
