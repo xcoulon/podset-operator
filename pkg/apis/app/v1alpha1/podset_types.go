9c8bb704c7fc631f1009e7683547a4571ddd2662
Xavier Coulon <xcoulon@redhat.com>
2019-11-15 11:31:32 +0100

    regenerate YAML and zz_generated with SDK 0.12
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- pkg/apis/app/v1alpha1/podset_types.go
+++ pkg/apis/app/v1alpha1/podset_types.go
@@ -7,6 +7,20 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +k8s:openapi-gen=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Namespaced
+
+// PodSet is the Schema for the podsets API
+type PodSet struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   PodSetSpec   `json:"spec,omitempty"`
+	Status PodSetStatus `json:"status,omitempty"`
+}
+
 // PodSetSpec defines the desired state of PodSet
 type PodSetSpec struct {
 	Replicas int32 `json:"replicas"`
@@ -15,18 +29,6 @@ type PodSetSpec struct {
 // PodSetStatus defines the observed state of PodSet
 type PodSetStatus struct {
 	PodNames []string `json:"podNames"`
-}
-
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-
-// PodSet is the Schema for the podsets API
-// +k8s:openapi-gen=true
-type PodSet struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Spec   PodSetSpec   `json:"spec,omitempty"`
-	Status PodSetStatus `json:"status,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
